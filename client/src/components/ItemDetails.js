64904b0c53a588b5eeb13910021947b673634dcd
Mabilog <g.angelo.m@gmail.com>
2022-05-30 14:26:25 -0400

    itemDetails page roughly done. just need to connect the backend and it should be good to go

--- client/src/components/ItemDetails.js
+++ client/src/components/ItemDetails.js
@@ -4,7 +4,19 @@ import styled from "styled-components";
 
 import { items } from "../data/items";
 const ItemDetails = () => {
-  console.log(items[0]);
+  /**
+   * Angelo's notes:
+   *
+   * I've created just a rought template on what would be relevant to the item details page.
+   * Feel free to go and change the styling to your hearts content.
+   *
+   * It would be wise to fetch information from the back as the component loads.
+   * - Taking the URL paramenter and doing a fetch using the item id.
+   * - .then assigning it to a local useState and displaying the information when the fetch is complete.
+   *
+   * I have provided what the fetched data should look like below
+   */
+
   /**
    * 
     "name": "Barska GB12166 Fitness Watch with Heart Rate Monitor",
@@ -12,7 +24,7 @@ const ItemDetails = () => {
     "body_location": "Wrist",
     "category": "Fitness",
     "_id": 6543,
-    "imageSrc": 
+    "imageSrc": <imageSrc information>
     "numInStock": 9,
     "companyId": 19962
    */
@@ -23,17 +35,17 @@ const ItemDetails = () => {
         <Right>
           <TitleWrapper>
             <h3>{items[0].name}</h3>
-            <span>id: {items[0]._id}</span>
+            <span>{items[0]._id}</span>
           </TitleWrapper>
+          <StockWrapper>
+            <button>{items[0].price} - Buy now</button>
+            <span>In Stock : {items[0].numInStock}</span>
+          </StockWrapper>
           <DetailsWrapper>
             <span>category: {items[0].category}</span>
             <span>body location: {items[0].body_location}</span>
             <span>made by: {items[0].companyId}</span>
           </DetailsWrapper>
-          <StockWrapper>
-            <button>{items[0].price} - </button>
-            <span>In Stock : {items[0].numInStock}</span>
-          </StockWrapper>
         </Right>
       </Wrapper>
     </ItemDetailsWrapper>
@@ -52,22 +64,49 @@ const Wrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  gap: 10px;
+  gap: 30px;
   padding: 20px;
 `;
 
 const Left = styled.div``;
 const Right = styled.div`
-  height: 175px;
+  height: 100%;
   display: flex;
   flex-direction: column;
   justify-content: space-around;
+  gap: 20px;
 `;
 
-const TitleWrapper = styled.div``;
+const TitleWrapper = styled.div`
+  max-width: 500px;
+  h3 {
+    font-weight: 600;
+    font-size: 26px;
+    line-height: 30px;
+    margin-top: 0;
+  }
+`;
 
-const DetailsWrapper = styled.div``;
+const StockWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
 
-const StockWrapper = styled.div``;
+  button {
+    padding: 15px 40px;
+    border: none;
+    border-radius: 10px;
+    background-color: #460fe0;
+    color: #fff;
+    font-size: 16px;
+    font-weight: 600;
+  }
+`;
+const DetailsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+`;
 
 export default ItemDetails;
