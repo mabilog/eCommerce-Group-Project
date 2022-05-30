01dd4c310b9d0d1c646bfa472ef9ad74848c4bc7
hersundaebest <sarahhuzarski@gmail.com>
2022-05-30 17:47:31 -0400

    adding header img and itemdetails styling

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,13 @@
 import styled from "styled-components";
 import ListingGrid from "./ListingGrid";
 import {items} from "../data/items"
+import HeaderImg from "./HeaderImg";
 
 
 const Home = (props) => {
     return (
         <>
+        <HeaderImg/>
 <ListingGrid itemList={Object.values(items)} />
         </>
     )
