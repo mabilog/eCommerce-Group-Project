d46cc2ab337e8c6900dbf598f00483f1334dca7b
Angelo Mab <g.angelo.m@gmail.com>
2022-05-30 09:14:43 -0400

    itemDetails still WIP

--- client/src/components/ItemDetails.js
+++ client/src/components/ItemDetails.js
@@ -5,22 +5,40 @@ import styled from "styled-components";
 import { items } from "../data/items";
 const ItemDetails = () => {
   console.log(items[0]);
+  /**
+   * 
+    "name": "Barska GB12166 Fitness Watch with Heart Rate Monitor",
+    "price": "$49.99",
+    "body_location": "Wrist",
+    "category": "Fitness",
+    "_id": 6543,
+    "imageSrc": 
+    "numInStock": 9,
+    "companyId": 19962
+   */
   return (
     <ItemDetailsWrapper>
-      <Left>
-        <img src={items[0].imageSrc} alt={items[0].name} srcset="" />
-      </Left>
-      <Center></Center>
-      <Right>
-        <div>
-          <h3>{items[0].name}</h3>
-          <p>{items[0].category}</p>
-        </div>
-        <div></div>
-        <div>
-          <button>{items[0].price} - </button>
-        </div>
-      </Right>
+      <Wrapper>
+        <Left>
+          <img src={items[0].imageSrc} alt={items[0].name} srcset="" />
+        </Left>
+        <Right>
+          <div>
+            <h3>{items[0].name}</h3>
+            <span>id: {items[0]._id}</span>
+            <span>{items[0].category}</span>
+          </div>
+          <div>
+            <span>category: {items[0].category}</span>
+            <span>body location: {items[0].body_location}</span>
+            <span>made by: {items[0].companyId}</span>
+          </div>
+          <div>
+            <button>{items[0].price} - </button>
+            <span>In Stock : {items[0].numInStock}</span>
+          </div>
+        </Right>
+      </Wrapper>
     </ItemDetailsWrapper>
   );
 };
@@ -33,8 +51,19 @@ const ItemDetailsWrapper = styled.div`
   height: 100%;
 `;
 
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+  padding: 20px;
+`;
+
 const Left = styled.div``;
-const Center = styled.div``;
-const Right = styled.div``;
+const Right = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
 
 export default ItemDetails;
